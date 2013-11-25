82ec42845c589fdc0bf7a475d04dc4f297be65ae
dustsnow <dustsnow@gmail.com>
2013-11-24 21:21:47 -0500

    Init; Able to receive data from named_pipe_server

---
+++ named_pipe_client_cs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Pipes;
+using System.IO;
+namespace named_pipe_client_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NamedPipeClientStream client = new NamedPipeClientStream(".", "my_pipe", PipeDirection.InOut);
+            client.Connect();
+            if (!client.IsConnected)
+            {
+                System.Console.Write("Failed to connect.");
+                return;
+            }
+            System.Console.Write("Connected.");
+            StreamReader sr = new StreamReader(client);
+            char[] c = new char[200];
+
+            while (sr.Peek() >= 0)
+            {
+                sr.Read(c, 0, c.Length);
+            }
+            string s = new string(c);
+            System.Console.Write(s);
+            System.Console.Read();
+        }
+    }
+}
